5c937a82bc9f716ff96aa69ec87f8618bf4928ac
tcerdaITBA <tcerda@itba.edu.ar>
2016-05-21 02:18:58 -0300

    AVLMap, HashMap y MinHeap

---
+++ TPE/src/structures/SimpleHashMap.java
@@ -0,0 +1,106 @@
+package structures;
+
+import java.lang.reflect.Array;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.Set;
+
+public class SimpleHashMap<K, V> implements SimpleMap<K, V> {
+	private static final int DEFAULT_CAPACITY = 20;
+
+	private int size;
+	private AVLMap<K,V>[] buckets;
+	private int mod;
+
+	@SuppressWarnings("unchecked")
+	public SimpleHashMap(int capacity, Comparator<K> comparator) {
+		if (capacity < 1)
+			throw new IllegalArgumentException("Illegal capacity < 1");
+
+		mod = capacity;
+		buckets = (AVLMap[]) Array.newInstance(AVLMap.class, capacity);
+
+		for (int i = 0; i < buckets.length; i++)
+			buckets[i] = new AVLMap<>(comparator);
+	}
+
+	public SimpleHashMap(Comparator<K> comparator) {
+		this(DEFAULT_CAPACITY, comparator);
+	}
+
+	@Override
+	public void put(K key, V value) {
+		int i = hash(key);
+		int prevSize = buckets[i].size();
+		buckets[i].put(key, value);
+		if (prevSize != buckets[i].size())
+			size += 1;
+	}
+
+	@Override
+	public V get(K key) {
+		int i = hash(key);
+		return buckets[i].get(key);
+	}
+
+	@Override
+	public boolean containsKey(K key) {
+		int i = hash(key);
+		return buckets[i].containsKey(key);
+	}
+
+	@Override
+	public void remove(K key) {
+		int i = hash(key);
+		int prevSize = buckets[i].size();
+		buckets[i].remove(key);
+		if (prevSize != buckets[i].size())
+			size -= 1;
+	}
+
+	@Override
+	public Set<K> keySet() {
+		Set<K> set = buckets[0].keySet();
+		for (int i = 1; i < buckets.length; i++)
+			set.addAll(buckets[i].keySet());
+		return set;
+	}
+
+	@Override
+	public Collection<V> values() {
+		Collection<V> collection = buckets[0].values();
+		for (int i = 1; i < buckets.length; i++)
+			collection.addAll(buckets[i].values());
+		return collection;
+	}
+
+	@Override
+	public int size() {
+		return size;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return size() == 0;
+	}
+
+	@Override
+	public void clear() {
+		size = 0;
+		for (AVLMap<K,V> bucket : buckets)
+			bucket.clear();
+	}
+
+	@Override
+	public boolean containsValue(V value) {
+		for (AVLMap<K,V> bucket : buckets)
+			if (bucket.containsValue(value))
+				return true;
+		return false;
+	}
+
+	private int hash(Object key) {
+		int i = key.hashCode() % mod;
+		return i > 0 ? i : -i;
+	}
+}
---
+++ TPE/src/structures/SimpleMap.java
@@ -0,0 +1,58 @@
+package structures;
+
+import java.util.Collection;
+import java.util.Set;
+
+public interface SimpleMap<K, V> {
+	/**
+	 * Agrega un par clave/valor a la tabla. Si la clave ya existe,
+	 * actualiza el valor.
+	 */
+	public void put(K key, V value);
+
+	/**
+	 * Obtiene el valor asociado a una clave. Si no existe la clave retorna null.
+	 */
+	public V get(K key);
+
+	/**
+	 * Devuelve verdadero si el mapa contiene la clave especificada.
+	 */
+	public boolean containsKey(K key);
+
+	/**
+	 * Retorna true si contiene una o más claves asociadas al valor.
+	 */
+	public boolean containsValue(V value);
+
+	/**
+	 * Elimina un par clave/valor de la tabla. Si la clave no existe no hace nada.
+	 */
+	public void remove(K key);
+
+	/**
+	 * Retorna todas las claves insertadas en la tabla.
+	 */
+	public Set<K> keySet();
+
+	/**
+	 * Retorna todos los valores insertados en la tabla.
+	 */
+	public Collection<V> values();
+
+
+	/**
+	 * Obtiene la cantidad de pares clave/valor insertados en la tabla.
+	 */
+	public int size();
+
+	/**
+	 * Retorna true si no existen pares clave/valor.
+	 */
+	public boolean isEmpty();
+
+	/**
+	 * Vacía el mapa
+	 */
+	public void clear();
+}
