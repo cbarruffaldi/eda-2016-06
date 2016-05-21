118b1e30147648a3ad79b23350e57779a8e211f0
marcelolynch <marcelomlynch@gmail.com>
2016-05-21 18:22:26 -0300

    Validaciones regex rout

--- TPE/src/utils/RegexHelper.java
+++ TPE/src/utils/RegexHelper.java
@@ -16,12 +16,12 @@ public class RegexHelper {
 
 	private static String spc = " ";
 	private static String airlineName = "[a-zA-Z]{1," + AIRLINE_NAME_MAX_LENGHT + "}";
-	private static String integer = "[0-9]+";
+	private static String number = "[0-9]+";
 	private static String days = "(Lu|Ma|Mi|Ju|Vi|Sa|Do)(-(Lu|Ma|Mi|Ju|Vi|Sa|Do))*"; // "Lu" o bien "Lu-Mi-Ju", etc
 	private static String airportName = "[a-zA-Zn—]{" + AIRPORT_NAME_LENGHT + "}";
 	private static String twentyFourHourFormat = "([01]?[0-9]|2[0-3]):[0-5][0-9]"; //00:00 - 23:59
 	private static String hours = "([0-9]+h)?[0-5][0-9]m"; // [xxh]xxm
-	private static String realNum = "[0-9]+(\\.[0-9]+)?";
+	private static String realPositiveNum = "[0-9]+(\\.[0-9]+)?";
 	
 	private static String latitude = "-?([0-8]?[0-9](\\.[0-9]+)?|90(\\.0+)?)"; //0.000 - 90.0000
 	private static String longitude = "-?(([0-9]|1[0-7])[0-9](\\.[0-9]+)?|180(\\.0+)?)"; //0.000 - 180.0000
@@ -30,30 +30,51 @@ public class RegexHelper {
 	
 	
     public static boolean validateFlightInsertion(String line) {
-    	String flightFormat = airlineName + spc + integer + spc + days + spc + airportName + spc + airportName +
-    			spc + twentyFourHourFormat + spc + hours + spc + realNum;
+    	String flightFormat = airlineName + spc + number + spc + days + spc + airportName + spc + airportName +
+    			spc + twentyFourHourFormat + spc + hours + spc + realPositiveNum;
+    	
     	//String regex = "[a-zA-Z]{1,3} [0-9]+ (Lu|Ma|Mi|Ju|Vi|Sa|Do)(-(Lu|Ma|Mi|Ju|Vi|Sa|Do))* [a-zA-ZÒ—]{3} [a-zA-ZÒ—]{3} ([01]?[0-9]|2[0-3]):[0-5][0-9] ([0-9]+h)?[0-5][0-9]m [0-9]+(\\.[0-9]+)?";
 
     	return line.matches(flightFormat);
 	}
     
+    public static boolean validateFlightName(String line) {
+    	String flightNameFormat = airlineName + spc + number;
+    	
+    	return line.matches(flightNameFormat);
+	}
+
     
     public static boolean validateAirportInsertion(String line) {
     	String airportFormat = airportName + spc + latitude + spc + longitude;
     	return line.matches(airportFormat);
 	}
     
+
+    //TODO: Testing
+    public static boolean validateRoute(String line) {
+    	String routeFormat = "src=" + airportName + spc + "dst=" + airportName 
+    			+ spc + "(priority=(ft|pr|tt))*" + "( weekdays=" + days + ")?";
+    	
+    	return line.matches(routeFormat);
+	}
+    
+
     
 
     
     public static void main(String[] args) {
         String flight = "AA 132 Lu-Ju-Mi-Sa BUE P—R 18:19 14h45m 1850.00";
-        System.out.println(validateFlight(flight));
+        System.out.println(validateFlightInsertion(flight));
         
         String coordinate = "-34.602535 -58.368731";
         
         System.err.println(coordinate.matches(latitude + spc + longitude));
         		
+        
+        String route = "src=BUE dst=LON priority=pr";
+        System.err.println(validateRoute(route));
+        
         
         Scanner sc = new Scanner(flight);
         
