7315c6f9bbcf2de2d79ad54b30e28ee1c8343689
marcelolynch <marcelomlynch@gmail.com>
2016-05-22 21:57:07 -0300

    Limpieza

--- TPE/src/utils/RegexHelper.java
+++ TPE/src/utils/RegexHelper.java
@@ -4,8 +4,6 @@ import java.util.Scanner;
 
 /**
  * Serie de metodos estaticos usando expresiones regulares para validar la entrada
- * @author Marcelo
- *
  */
 public class RegexHelper {
 	
@@ -14,6 +12,8 @@ public class RegexHelper {
     public static final double MAX_LATITUDE = 90.0;
     public static final double MAX_LONGITUDE = 180.0;
 
+    
+    //Atomos para las expresiones usadas
 	private static String spc = " ";
 	private static String airlineName = "[a-zA-Z]{1," + AIRLINE_NAME_MAX_LENGHT + "}";
 	private static String number = "[0-9]+";
@@ -26,35 +26,42 @@ public class RegexHelper {
 	private static String latitude = "-?([0-8]?[0-9](\\.[0-9]+)?|90(\\.0+)?)"; //0.000 - 90.0000
 	private static String longitude = "-?(([0-9]|1[0-7])?[0-9](\\.[0-9]+)?|180(\\.0+)?)"; //0.000 - 180.0000
 
-    // Se usa de igual manera para leer de línea de comandos o de archivo, con la única diferencia que en
-    // los archivos se separan las palabras por medio de "#".
+    
+	
+	
+	// Se usa de igual manera para leer de linea de comandos o de archivo, con la unica diferencia 
+	//que en los archivos se separan las palabras por medio de "#".
     public static boolean validateFlightInsertion(String line, String separator) {
     	String flightFormat = airlineName + separator + number + separator + days + separator + airportName + separator + airportName +
     			separator + twentyFourHourFormat + separator + hours + separator + realPositiveNum;
     	
-    	//String regex = "[a-zA-Z]{1,3} [0-9]+ (Lu|Ma|Mi|Ju|Vi|Sa|Do)(-(Lu|Ma|Mi|Ju|Vi|Sa|Do))* [a-zA-Z��]{3} [a-zA-Z��]{3} ([01]?[0-9]|2[0-3]):[0-5][0-9] ([0-9]+h)?[0-5][0-9]m [0-9]+(\\.[0-9]+)?";
-
     	return line.matches(flightFormat);
 	}
     
+
+    
+    //Static para no armarla cada vez
+    private static String flightNameFormat = airlineName + spc + number;
+    
     public static boolean validateFlightName(String line) {
-    	String flightNameFormat = airlineName + spc + number;
-    	
     	return line.matches(flightNameFormat);
 	}
 
+ 
+    
     public static boolean validateAirportInsertion(String line, String separator) {
     	String airportFormat = airportName + separator + latitude + separator + longitude;
     	return line.matches(airportFormat);
 	}
     
 
-    //TODO: Testing
-    public static boolean validateRoute(String line) {
-    	String routeFormat = "src=" + airportName + spc + "dst=" + airportName 
-    			+ spc + "priority=(ft|pr|tt)" + "( weekdays=" + days + ")?";
+
+    //Static para no armarla cada vez
+    private static String routeFormat = "src=" + airportName + spc + "dst=" + airportName 
+			+ spc + "priority=(ft|pr|tt)" + "( weekdays=" + days + ")?";
     
-    	return line.matches(routeFormat);
+    public static boolean validateRoute(String line) {
+        	return line.matches(routeFormat);
 	}
     
 	public static boolean validateAirportName(String line) {
@@ -62,43 +69,6 @@ public class RegexHelper {
 	}
 
    
-    public static void main(String[] args) {
-        String flight = "AA 132 Lu-Ju-Mi-Sa BUE P�R 18:19 14h45m 1850.00";
-        System.out.println(validateFlightInsertion(flight, spc));
-        
-        String coordinate = "-34.602535 -58.368731";
-        
-        System.err.println(coordinate.matches(latitude + spc + longitude));
-        		
-        
-        String route = "src=BUE dst=LON priority=pr weekdays=Lu-Mi-Vi";
-        
-        
-        Scanner sc = new Scanner(flight);
-        
-        
-        String airline = sc.next();
-        int flnumber = new Integer(sc.next());
-        String days = sc.next();
-        String orig = sc.next();
-        String dest = sc.next();
-        String timeOfDeparture = sc.next();
-        String durationOfFlight = sc.next();
-        double price = new Double(sc.next());
-        
-        
-       System.out.println(airline);
-       System.out.println(flnumber);
-       System.out.println(days);
-       System.out.println(orig);
-       System.out.println(dest);
-       System.out.println(timeOfDeparture);
-       System.out.println(durationOfFlight);
-       System.out.println(price);
-
-    }
-
-    
-    
+     
 
 }
