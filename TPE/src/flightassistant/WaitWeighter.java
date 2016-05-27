a5052d490f6f8edee9024f936bb3835675e53adb
Carla Barruffaldi <cbarruffaldi@itba.edu.ar>
2016-05-27 19:35:32 -0300

    Commit Grupal !

---
+++ TPE/src/flightassistant/WaitWeighter.java
@@ -0,0 +1,13 @@
+package flightassistant;
+
+public class WaitWeighter {
+	
+	@Override
+	public WeightedFlight minFlight(Airport from, Airport to) {
+		A
+		Flight shortest = from.getShortestTo(to);
+		Airport
+		return new WeightedFlight(quickest, );
+	}
+	
+}
